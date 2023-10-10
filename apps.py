90a128323a6df89af36684431867586994dbce5a
filyuya <143702119+filyuya@users.noreply.github.com>
2023-10-10 22:13:55 +0400

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AppScaramoucheConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_scaramouche'
+    verbose_name = 'Объявления'
